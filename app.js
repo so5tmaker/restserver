1f7e83815c550a87019f2317d2efaca9012d1443
Viktor Sivulskiy <svicvg@gmail.com>
2018-01-25 16:07:11 +0600

    Express REST API with MongoDB and Mongoose Part 2

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const promoRouter = require('./routes/promoRouter');
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+// Connection URL
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+    useMongoClient: true,
+    /* other options */
+  });
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 var app = express();
 
 // view engine setup
