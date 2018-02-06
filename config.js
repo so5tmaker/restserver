2bc97d0952bd778e66bdec4a44528b9fe3c77d67
Viktor Sivulskiy <svicvg@gmail.com>
2018-02-06 15:23:13 +0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '416544215432923',
+        clientSecret: '7ce4ecd8748b0806e635852c2f793d04'
+    }
 }
